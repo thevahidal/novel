f96cf61a861565626db9d0cefab83adb6e483df6
Vahid Al <thevahidal@gmail.com>
2022-12-01 01:51:32 +0330

    google analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Script from 'next/script';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { usePanelbear } from '@panelbear/panelbear-nextjs';
@@ -11,14 +12,34 @@ function MyApp({ Component, pageProps }) {
   usePanelbear(process.env.NEXT_PUBLIC_PANEL_BEAR_ID);
 
   return (
-    <SessionContextProvider
-      supabaseClient={supabase}
-      initialSession={pageProps.initialSession}
-    >
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionContextProvider>
+    <>
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id='google-analytics'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <SessionContextProvider
+        supabaseClient={supabase}
+        initialSession={pageProps.initialSession}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionContextProvider>
+    </>
   );
 }
 
